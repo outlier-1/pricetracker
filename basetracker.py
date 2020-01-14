b0532b57c33d0e7c58cfea2d0317c64c37b803d2
Miraç Baydemir <omermirac59@gmail.com>
2020-01-15 02:08:59 +0300

    Add Feature: HB Get Price From Link

--- basetracker.py
+++ basetracker.py
@@ -1,2 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 class Base(object):
+
+    @staticmethod
+    def get_soup(link):
+        html = requests.get(link).content
+        return BeautifulSoup(html, features='html.parser')
+
+    def get_price_from_link(self, link):
+        pass
+
+    def search_product(self, *args, **kwargs):
+        pass
+    
     pass
--- tracker_hb.py
+++ tracker_hb.py
@@ -1,5 +1,14 @@
 from basetracker import Base
-
+from bs4 import BeautifulSoup
+import codecs
+from utilities import char_to_hex
 
 class HBTracker(Base):
-    pass
+
+    SEARCH_QUERY_BASE = 'https://www.hepsiburada.com/ara?q='
+
+    def get_price_from_link(self, link):
+        """ <span id='offering-id' content='PRICE' """
+        soup = Base.get_soup(link)
+        price_tag = soup.find(name='span', id='offering-price')
+        return price_tag['content']
